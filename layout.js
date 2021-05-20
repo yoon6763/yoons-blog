f2141e970fa1f5b37067bdd2f6f9d6383604ba29
yoon <yoon6763@naver.com>
2021-05-20 23:49:21 +0900

    version 2.0

---
+++ layout.js
@@ -0,0 +1,18 @@
+// 네비게이션 바 영역
+
+document.getElementById("tap1").innerHTML="Main";
+document.getElementById("tap2").innerHTML="Profile";
+document.getElementById("tap3").innerHTML="Project";
+document.getElementById("tap4").innerHTML="About Me";
+
+
+
+// footer 영역
+
+document.getElementById("footertext").innerHTML
+    = "<br>&nbsp&nbsp&nbsp&nbsp<b>Yoon's Blog</b>&nbsp ─ &nbspDeveloped by <b>전윤호</b><br>"
+    + "<br>&nbsp&nbsp&nbsp&nbsp<b>State</b> &nbsp─&nbsp Yongin, Republic of Korea &nbsp|&nbsp"
+    + "<b>e-mail</b> &nbsp─&nbsp yoon6763@naver.com &nbsp|&nbsp "
+    + "<b>Blog</b> &nbsp─&nbsp https://blog.naver.com/yoon6763 &nbsp|&nbsp "
+    + "<b>GitHub</b> &nbsp─&nbsp https://github.com/yoon6763<br>"
+    + "<br>&nbsp&nbsp&nbsp&nbsp<b>Copyrightⓒ2021</b> by 전윤호 All rights reserved.<br>&nbsp";
